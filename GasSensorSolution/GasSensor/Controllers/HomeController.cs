2aee3e9fa3ae56fa87d09b7d4447bcf262a6e7f6
DiegoSampaio <dsampaio@mundipagg.com>
2015-11-13 12:47:45 -0200

    :art: Implementa o controlador principal e algumas views

--- GasSensorSolution/GasSensor/Controllers/HomeController.cs
+++ GasSensorSolution/GasSensor/Controllers/HomeController.cs
@@ -3,20 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GasSensor.Utilities;
 
 namespace GasSensor.Controllers
 {
+    /// <summary>
+    /// Controlador principal
+    /// </summary>
     public class HomeController : Controller
     {
+        // Instanciando a classe que faz a lógica do envio de e-mail.
+        SendMail eMail = new SendMail();
+
+
         // GET: Home
         public ActionResult Index()
         {
-            return SendMail();
+            return View();
         }
 
-        // GET: Home
+
+        // GET: Simulator
+        public ActionResult Simulator()
+        {
+            // Inicia o gerador randomico de números
+            Random rnd = new Random();
+
+            // Gera um numero aleatório de 0 a 10
+            int numero = rnd.Next(0, 10);
+
+            // Caso o teor de vazamento de gás seja maior que 1 será disparado um e-mail avisando que está ocorrendo um vazamento de gás
+            if (numero > 1)
+            {
+                // Redirecionado para a ação de enviar o e-mail
+                return RedirectToAction("SendMail");
+
+            }
+            else
+            {
+                // Redireciona de volta para a pagina principal
+                return RedirectToAction("Index");
+            }
+
+        }
+
+        // GET: SendMail
         public ActionResult SendMail()
         {
+            // Instancia o método de envio de e-mail.
+            eMail.SendMessage();
+
+            // Vai para a view correspondente.
             return View();
         }
     }
--- GasSensorSolution/GasSensor/Utilities/SendMail.cs
+++ GasSensorSolution/GasSensor/Utilities/SendMail.cs
@@ -6,25 +6,56 @@ using System.Web;
 
 namespace GasSensor.Utilities
 {
+    /// <summary>
+    /// Classe para enviar o e-mail
+    /// </summary>
     public class SendMail
     {
+        /// <summary>
+        /// Método de envio de e-mail
+        /// </summary>
         public void SendMessage()
         {
+            // Classe de envio de e-mail utilizando SMTP
             SmtpClient client = new SmtpClient();
+            
+            // Definindo o host a ser utilizado
             client.Host = "smtp.gmail.com";
+            
+            // Encriptografia em SSL habilitada
             client.EnableSsl = true;
+
+            // Passando as credenciais de acesso do e-mail a ser utilizado
             client.Credentials = new System.Net.NetworkCredential("diego.dscrj@gmail.com", "@Ie14ih153");
+
+            // Instancia a classe de envio.
             MailMessage mail = new MailMessage();
+
+            // Especifica o e-mail de quem está enviando (opicional)
             mail.Sender = new MailAddress("diego.dscrj@gmail.com", "Sender");
+
+            // Especifica quem está enviando o e-mail
             mail.From = new MailAddress("diego.dscrj@gmail.com", "Quem enviou");
+
+            // Especifica os remetentes do e-mail
             mail.To.Add(new MailAddress("dsampaio@mundipagg.com", "Receiver"));
+
+            // Assunto do e-mail
             mail.Subject = "Contato";
+
+            // Corpo do e-mail
             mail.Body = " Mensagem do site: E-mail de teste para o cliente de envio de e-mail do trabalho da Yona.<br/> Nome:  " + "Diego" + "<br/> Email : " + "diego.dscrj@gmail.com" +
                         " <br/> Mensagem : " + "Mensagem de teste";
+
+            // Diz que o corpo do e-mail é um HTML
             mail.IsBodyHtml = true;
+
+            // Seta a prioridade do e-mail
             mail.Priority = MailPriority.High;
+
             try
             {
+                // Envia o e-mail
                 client.Send(mail);
             }
             catch (Exception erro)
@@ -33,6 +64,7 @@ namespace GasSensor.Utilities
             }
             finally
             {
+                // Esvazia o e-mail enviado.
                 mail = null;
             }
 
