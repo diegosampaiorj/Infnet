5986003059674c753a05b3e491d38eb075e72ae9
DiegoSampaio <dsampaio@mundipagg.com>
2015-11-13 11:56:44 -0200

    :art: Adcionando Random no POC e criando a solução do vazamento de gas

---
+++ GasSensorSolution/GasSensor/Utilities/SendMail.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+
+namespace GasSensor.Utilities
+{
+    public class SendMail
+    {
+        public void SendMessage()
+        {
+            SmtpClient client = new SmtpClient();
+            client.Host = "smtp.gmail.com";
+            client.EnableSsl = true;
+            client.Credentials = new System.Net.NetworkCredential("diego.dscrj@gmail.com", "@Ie14ih153");
+            MailMessage mail = new MailMessage();
+            mail.Sender = new MailAddress("diego.dscrj@gmail.com", "Sender");
+            mail.From = new MailAddress("diego.dscrj@gmail.com", "Quem enviou");
+            mail.To.Add(new MailAddress("dsampaio@mundipagg.com", "Receiver"));
+            mail.Subject = "Contato";
+            mail.Body = " Mensagem do site: E-mail de teste para o cliente de envio de e-mail do trabalho da Yona.<br/> Nome:  " + "Diego" + "<br/> Email : " + "diego.dscrj@gmail.com" +
+                        " <br/> Mensagem : " + "Mensagem de teste";
+            mail.IsBodyHtml = true;
+            mail.Priority = MailPriority.High;
+            try
+            {
+                client.Send(mail);
+            }
+            catch (Exception erro)
+            {
+                throw erro;
+            }
+            finally
+            {
+                mail = null;
+            }
+
+        }
+    }
+}
