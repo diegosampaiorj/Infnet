3371dc79e57803386a61e08753c9b65e94354983
DiegoSampaio <dsampaio@mundipagg.com>
2015-11-13 11:29:14 -0200

    primeiro

---
+++ POCSendMail/POCSendMail/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POCSendMail
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            SendMail eMail = new SendMail();
+
+           
+            Console.WriteLine("Vou enviar o e-mail, aperte 1 para confirmar ou 2 para não enviar.");
+
+            int envio = Console.Read();
+
+            if (envio == 49)
+            {
+                eMail.SendMessage();
+
+                Console.WriteLine("E-mail enviado com sucesso");
+            }
+            else
+            {
+                Console.WriteLine("Envio de e-mail cancelado");
+            }
+        }
+    }
+}
